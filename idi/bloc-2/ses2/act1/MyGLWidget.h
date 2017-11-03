42b7c9217d762692decbd136c65211a74c1065be
Joan Pad <thevader33@gmail.com>
2017-11-04 00:12:48 +0100

    session 5

---
+++ idi/bloc-2/ses2/act1/MyGLWidget.h
@@ -0,0 +1,69 @@
+#define GLM_FORCE_RADIANS
+#include <QOpenGLFunctions_3_3_Core>
+#include <QOpenGLWidget>
+#include <QOpenGLShader>
+#include <QOpenGLShaderProgram>
+#include <QKeyEvent>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "model.h"
+
+class MyGLWidget : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core
+{
+  Q_OBJECT
+
+  public:
+    MyGLWidget (QWidget *parent=0);
+    ~MyGLWidget ();
+
+  protected:
+    // initializeGL - Aqui incluim les inicialitzacions del contexte grafic.
+    virtual void initializeGL ( );
+    // paintGL - Mètode cridat cada cop que cal refrescar la finestra.
+    // Tot el que es dibuixa es dibuixa aqui.
+    virtual void paintGL ( );
+    // resizeGL - És cridat quan canvia la mida del widget
+    virtual void resizeGL (int width, int height);
+    // keyPressEvent - Es cridat quan es prem una tecla
+    virtual void keyPressEvent (QKeyEvent *event);
+
+
+  private:
+    void createBuffers ();
+    void carregaShaders ();
+    void modelTransform ();
+    void modelTransformTerra();
+    void projectTransform();
+    void evitaDeformacions(int w, int h);
+    void viewTransform();
+    void ini_camera();
+    void carregaModelHomer();
+    void pinta_homer();
+    void pinta_terra();
+    // attribute locations
+    GLuint vertexLoc, colorLoc;
+    // uniform locations
+    GLuint transLoc;
+    GLuint projLoc;
+    GLuint viewLoc;
+    // VAO i VBO names
+    GLuint VAO_Homer, VBO_HomerPos, VBO_HomerCol, VAO_Terra, VBO_TerraPos, VBO_TerraCol;
+    // Program
+    QOpenGLShaderProgram *program;
+    // Internal vars
+
+    Model m;
+
+    float scale;
+    float angle;
+    float FOV;
+    float FOVini;
+    float ra;
+    float znear;
+    float zfar;
+    glm::vec3 pos;
+    glm::vec3 OBS;
+    glm::vec3 VRP;
+    glm::vec3 UP;
+
+};
---
+++ idi/bloc-2/ses2/act5/MyGLWidget.cpp
@@ -0,0 +1,334 @@
+#include "MyGLWidget.h"
+
+#include <iostream>
+
+MyGLWidget::MyGLWidget (QWidget* parent) : QOpenGLWidget(parent)
+{
+  setFocusPolicy(Qt::ClickFocus);  // per rebre events de teclat
+  scale = 1.0f;
+}
+
+MyGLWidget::~MyGLWidget ()
+{
+  if (program != NULL)
+    delete program;
+}
+
+void MyGLWidget::initializeGL ()
+{
+  // Cal inicialitzar l'ús de les funcions d'OpenGL
+  initializeOpenGLFunctions();
+  psi = 0.0f;
+  theta = 0.0f;
+  glClearColor(0.5, 0.7, 1.0, 1.0); // defineix color de fons (d'esborrat)
+  carregaShaders();
+  createBuffers();
+  glEnable(GL_DEPTH_TEST);
+  ini_camera();
+}
+
+void MyGLWidget::paintGL ()
+{
+  // Esborrem el frame-buffer
+  glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  // Carreguem la transformació de model
+  modelTransform();
+  pinta_homer();
+  //modelTransformTerra();
+  //pinta_terra();
+
+  glBindVertexArray (0);
+}
+
+void MyGLWidget::resizeGL (int w, int h)
+{
+  evitaDeformacions(w, h);
+  glViewport(0, 0, w, h);
+}
+
+void MyGLWidget::createBuffers ()
+{
+    carregaModelHomer();
+    //carregaTerra();
+    calculcapsacontainer();
+    calculcapsaminima();
+    centrarmodel();
+    glBindVertexArray (0);
+}
+
+void MyGLWidget::carregaShaders()
+{
+  // Creem els shaders per al fragment shader i el vertex shader
+  QOpenGLShader fs (QOpenGLShader::Fragment, this);
+  QOpenGLShader vs (QOpenGLShader::Vertex, this);
+  // Carreguem el codi dels fitxers i els compilem
+  fs.compileSourceFile("shaders/fragshad.frag");
+  vs.compileSourceFile("shaders/vertshad.vert");
+  // Creem el program
+  program = new QOpenGLShaderProgram(this);
+  // Li afegim els shaders corresponents
+  program->addShader(&fs);
+  program->addShader(&vs);
+  // Linkem el program
+  program->link();
+  // Indiquem que aquest és el program que volem usar
+  program->bind();
+
+  // Obtenim identificador per a l'atribut “vertex” del vertex shader
+  vertexLoc = glGetAttribLocation (program->programId(), "vertex");
+  // Obtenim identificador per a l'atribut “color” del vertex shader
+  colorLoc = glGetAttribLocation (program->programId(), "color");
+  // Uniform locations
+  transLoc = glGetUniformLocation(program->programId(), "TG");
+  projLoc = glGetUniformLocation(program->programId(), "proj");
+  viewLoc = glGetUniformLocation(program->programId(), "view");
+}
+
+
+/*
+        ***********************
+        *                     *
+        *    MIS FUNCIONES    *
+        *                     *
+        ***********************
+
+*/
+
+/*
+ ************************************
+ *                                  *
+ *   FUNCIONES DE CARREGA DE VAOs   *
+ *                                  *
+ ************************************
+*/
+
+void MyGLWidget::carregaModelHomer()
+{
+    m.load("../../models/Patricio.obj");
+
+    // Creació del Vertex Array Object per pintar
+    glGenVertexArrays(1, &VAO_Patricio);
+    glBindVertexArray(VAO_Patricio);
+
+    glGenBuffers(1, &VBO_PatricioPos);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_PatricioPos);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*m.faces().size()*3*3, m.VBO_vertices(), GL_STATIC_DRAW);
+
+    // Activem l'atribut vertexLoc
+    glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(vertexLoc);
+
+    glGenBuffers(1, &VBO_PatricioCol);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_PatricioCol);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*m.faces().size()*3*3, m.VBO_matdiff(), GL_STATIC_DRAW);
+
+    // Activem l'atribut colorLoc
+    glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(colorLoc);
+
+}
+
+void MyGLWidget::carregaTerra(){
+  glm::vec3 posicio[4] = {
+    glm::vec3(1.0, -1.0, 1.),
+    glm::vec3(-1.0, -1.0, 1.0),
+    glm::vec3(1.0, -1.0, -1.0),
+    glm::vec3(-1.0, -1.0, -1.0)
+  };
+  glm::vec3 Colors[3];  // Tres vèrtexs amb X, Y i Z
+  Colors[0] = glm::vec3(1.0, 0.0, 0.0);
+  Colors[1] = glm::vec3(0.0, 1.0, 0.0);
+  Colors[2] = glm::vec3(0.0, 0.0, 1.0);
+
+  glGenVertexArrays(1, &VAO_Terra);
+  glBindVertexArray(VAO_Terra);
+
+  glGenBuffers(1, &VBO_TerraPos);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_TerraPos);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(posicio), posicio, GL_STATIC_DRAW);
+
+  // Activem l'atribut vertexLoc
+  glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(vertexLoc);
+
+  glGenBuffers(1, &VBO_TerraCol);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_TerraCol);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Colors), Colors, GL_STATIC_DRAW);
+
+  // Activem l'atribut vertexLoc
+  glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(colorLoc);
+}
+
+/*
+ ********************************
+ *                              *
+ *   FUNCIONES DE PINTAR VAOs   *
+ *                              *
+ ********************************
+*/
+
+void MyGLWidget::pinta_homer()
+{
+        // Activem el VAO per a pintar el homer
+    glBindVertexArray (VAO_Patricio);
+
+    // pintem
+    glDrawArrays(GL_TRIANGLES, 0, m.faces().size()*3);
+}
+
+void MyGLWidget::pinta_terra()
+{
+        // Activem el VAO per a pintar el homer
+    glBindVertexArray (VAO_Terra);
+
+    // pintem
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+}
+/*
+ ***************************
+ *                         *
+ *   FUNCIONES DE VISIÓN   *
+                           *
+ ***************************
+*/
+
+void MyGLWidget::projectTransform(){
+    //FOV, ra, znear, zfar
+    glm::mat4 Proj = glm::perspective(FOV,ra,znear,zfar);
+    glUniformMatrix4fv(projLoc,1,GL_FALSE,&Proj[0][0]);
+}
+
+void MyGLWidget::viewTransform(){
+    //glm::mat4 ViewMat = glm::lookAt(OBS,VRP,UP);
+    glm::mat4 euler (1.0f);
+    euler = glm::translate(euler, glm::vec3(0.f, 0.f, -distancia));
+    euler = glm::rotate(euler,theta,glm::vec3(1.0,0.0,0.0));
+    euler = glm::rotate(euler,-psi,glm::vec3(0.0,1.0,0.0));
+    euler = glm::translate(euler, -VRP);
+    std::cout<< "euler: "<<VRP[0]<< " " <<VRP[1]<<" "<<VRP[2]<<std::endl;
+    glUniformMatrix4fv(viewLoc,1,GL_FALSE,&euler[0][0]);
+}
+
+void MyGLWidget::ini_camera(){
+    FOV = FOVini = (float)atan(radi/distancia)*2;
+
+    ra = 1.0f;
+    znear = distancia-radi;
+    zfar = distancia+radi;
+    projectTransform();
+
+    //OBS = glm::vec3(0,0,3);
+    VRP = glm::vec3(0,0,0);
+    OBS = VRP;
+    OBS[2] += distancia;
+    UP = glm::vec3(0,1,0);
+    viewTransform();
+}
+
+void MyGLWidget::calculcapsacontainer(){
+  vectorminim[0] = vectormax[0] = (float)m.vertices()[0];
+  vectorminim[1] = vectormax[1] = (float)m.vertices()[1];
+  vectorminim[2] = vectormax[2] = (float)m.vertices()[2];
+
+  for (unsigned int i = 0; i < m.vertices().size(); i += 3){
+    //la x
+    if ((float)m.vertices()[i] > vectormax[0]) vectormax[0] = (float)m.vertices()[i];
+    else if ((float)m.vertices()[i] < vectorminim[0]) vectorminim[0] = (float)m.vertices()[i];
+
+    //la y
+    if ((float)m.vertices()[i+1] > vectormax[1]) vectormax[1] = (float)m.vertices()[i+1];
+    else if ((float)m.vertices()[i+1] < vectorminim[1]) vectorminim[1] = (float)m.vertices()[i+1];
+
+    //la z
+    if ((float)m.vertices()[i+2] > vectormax[2]) vectormax[2] = (float)m.vertices()[i+2];
+    else if ((float)m.vertices()[i+2] < vectorminim[2])vectorminim[2] = (float)m.vertices()[i+2];
+  }
+  std::cout << "("<< vectormax[0] << ", " << vectormax[1] << ", " << vectormax[2] << ")" << std::endl;
+}
+
+void MyGLWidget::calculcapsaminima(){
+  centrecaixa[0] = (vectormax[0]+vectorminim[0])/2;
+  centrecaixa[1] = (vectormax[1]+vectorminim[1])/2;
+  centrecaixa[2] = (vectorminim[2]+vectormax[2])/2;
+  float xaux = pow((vectormax[0] - vectorminim[0]),2);
+  float yaux = pow((vectormax[1] - vectorminim[1]),2);
+  float zaux = pow((vectormax[2] - vectorminim[2]),2);
+  float sumaux = xaux + yaux + zaux;
+  radi = sqrt(sumaux);
+  distancia = 2*radi;
+  std::cout << "radi: "<< radi << std::endl;
+  std::cout << "centre caixa: ("<< centrecaixa[0]<<", " << centrecaixa[1]<<", "<<centrecaixa[2]<<")"<< std::endl;
+
+  //radi = dist((vectorminim[0], vectorminim[1], vectorminim[2]),(vectormax[0], vectormax[1], vectormax[2]))/2;
+}
+
+void MyGLWidget::centrarmodel(){
+  glm::mat4 matTG(1.0); //Matriu identidat
+  glm::vec3 values = centrecaixa;
+  values*= -1.0f;
+  std::cout << "moviment: ("<<values[0]<<", "<< values[1]<< ", "<<values[2]<< ")"<< std::endl;
+  matTG = glm::translate (matTG,values);
+  glUniformMatrix4fv(transLoc,1,GL_FALSE,&matTG[0][0]);
+}
+
+/*
+ ***********************************
+ *                                 *
+ *   FUNCIONES DE TRANSFORMACIÓN   *
+ *                                 *
+ ***********************************
+*/
+
+void MyGLWidget::modelTransform ()
+{
+  // Matriu de transformació de model
+  glm::mat4 transform (1.0f);
+  transform = glm::scale(transform, glm::vec3(scale));
+  transform = glm::rotate(transform,angle,glm::vec3(0.0,1.0,0.0));
+  transform = glm::translate (transform,-centrecaixa);
+  glUniformMatrix4fv(transLoc, 1, GL_FALSE, &transform[0][0]);
+}
+
+void MyGLWidget::modelTransformTerra ()
+{
+  // Matriu de transformació de model
+  glm::mat4 transform (1.0f);
+  glUniformMatrix4fv(transLoc, 1, GL_FALSE, &transform[0][0]);
+}
+
+void MyGLWidget::evitaDeformacions(int w, int h){
+  ra = float(w)/float(h);
+  if (ra < 1.0f) FOV = 2 * atan(tan(FOVini/2)/ra);
+  projectTransform();
+}
+
+/*
+ ********************************
+ *                              *
+ *   FUNCIONES DE INTERACCIÓN   *
+ *                              *
+ ********************************
+*/
+
+void MyGLWidget::keyPressEvent(QKeyEvent* event)
+{
+  makeCurrent();
+  switch (event->key()) {
+    case Qt::Key_S: { // escalar a més gran
+      scale += 0.05;
+      break;
+    }
+    case Qt::Key_D: { // escalar a més petit
+      scale -= 0.05;
+      break;
+    }
+    case Qt::Key_R: { //rotacio 45 graus
+      angle += (float)(M_PI/4);
+      break;
+    }
+    default: event->ignore(); break;
+  }
+  update();
+}
